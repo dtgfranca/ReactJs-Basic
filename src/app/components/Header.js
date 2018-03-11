858b070b9b8a978b3ab31a250987cac494f1a95a
Diego França <diego.tg.franca@gmail.com>
2018-03-11 09:47:25 -0300

    primeiro commit

---
+++ src/app/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export class Header extends React.Component{
+	render(){
+		return(
+			<nav className="navbar navbar-default">
+			<div classNmae="container"> 
+			<div className="navbar-header">
+			<ul className="nav nav-bar-nav">
+			<li><a href="#">HOME</a></li>
+			</ul>
+			</div>
+
+			</div>
+			</nav>
+
+
+		);
+	}
+} 
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,18 @@
-console.log("it's work!!");
+import React from "react";
+import { render } from "react-dom";
+
+import {Header} from "./componets/Header";
+import {Home} from "./componets/Header";
+
+class App extends React.Component{
+	render(){
+		return(
+			<div>
+				<Header/>
+			</div>
+		);
+
+	}
+}
+
+render(<App/>, document.getElementById("app"));
